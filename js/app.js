86313fdee464123c97f2d7f63d4dd7fa84becd6a
Belgarde Jean Ronel <belganel@gmail.com>
2023-04-22 14:23:28 +0200

    Ajout de 'NEWSLETTER, MESSAGES D'ERREURS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import { createElement, eventClickMode, eventClickThemeColor, plusSlides, showSlides, slideIndex } from "./functions.js";
 
+// CHANGER DE THEME
+
 const themeSwitch = document.querySelector("#theme-switch");
 const body = document.querySelector("body");
 
@@ -24,6 +26,8 @@ const themeBlue = document.querySelector("#theme-blue");
 eventClickThemeColor(themeBlue, body, "theme-blue", "theme-green", "theme-red", img, logoImages[2]);
 
 
+
+// SLIDER
 
 const sliderImages = [
     "canyon.jpg",
@@ -56,5 +60,122 @@ const next = document.querySelectorAll(".slider__btn")[1];
 next.addEventListener("click", function() {
     plusSlides(1);
 });
+
+
+// NEWSLETTER
+
+const openNewsLetter = document.querySelector(".menu__item:nth-of-type(2)");
+const newsLetter = document.querySelector(".newsletter");
+const closeNewsLetter = document.querySelector(".newsletter__close");
+let indice = 0;
+
+openNewsLetter.addEventListener("click", function(event) {
+    event.preventDefault();
+    newsLetter.classList.add("newsletter--on");
+    indice ++;
+})
+
+closeNewsLetter.addEventListener("click", () => {
+    newsLetter.classList.remove("newsletter--on");
+    indice ++;
+})
+
+window.addEventListener("scroll", function() {
+    let scrl = this.scrollY;
+
+    if (!indice) {
+        if (scrl >= 300) {
+            newsLetter.classList.add("newsletter--on");
+        }else{
+            newsLetter.classList.remove("newsletter--on");
+        }
+    }
+})
+
+
+// MESSAGES D'ERREURS
+
+const forbiddenDomains = [
+    '@yopmail.com',
+    '@yopmail.fr',
+    '@yopmail.net',
+    '@cool.fr.nf',
+    '@jetable.fr.nf',
+    '@courriel.fr.nf',
+    '@moncourrier.fr.nf',
+    '@monemail.fr.nf',
+    '@monmail.fr.nf',
+    '@hide.biz.st',
+    '@mymail.infos.st',
+]
+
+const formNewsLetter = document.forms[0];
+
+const spanErrorEmail = createElement("span");
+spanErrorEmail.textContent = "Email incorrect!";
+
+spanErrorEmail.style.cssText = `color: red;
+    display: none;
+    margin-top: 10px;
+`;
+formNewsLetter.append(spanErrorEmail);
+
+const input = document.querySelector(".newsletter__field");
+
+formNewsLetter.addEventListener("submit", event => {
+
+    for (const email of forbiddenDomains) {
+        if (input.value.endsWith(email)){
+            event.preventDefault();
+            spanErrorEmail.style.display = "inline-block";
+        }else{
+            spanErrorEmail.style.display = "none";
+        }
+    }
+})
+
+input.addEventListener("focus", () => {
+    spanErrorEmail.style.display = "none";
+})
+
+
+// FAVORIS
+
+
+const spanErrorFavori = createElement("span");
+spanErrorFavori.textContent = "Vous devez être connecté pour gérer vos favoris";
+
+spanErrorFavori.style.cssText = `color: red;
+    display: none;
+`;
+
+const articles = document.querySelectorAll(".card");
+articles.forEach(article => {
+    const spanErrorFavori = createElement("span");
+    spanErrorFavori.textContent = "Vous devez être connecté pour gérer vos favoris";
+
+    spanErrorFavori.style.cssText = `color: red;
+        display: none;
+    `;
+
+    article.prepend(spanErrorFavori);
+})
+
+
+
+const favoris = document.querySelectorAll(".btn.btn__like");
+
+favoris.forEach(favori => {
+    favori.addEventListener("click", () => {
+        favori.parentElement.parentElement.querySelector("span").style.display = "block";
+
+        setTimeout(function(){
+            favori.parentElement.parentElement.querySelector("span").style.display = "none";
+        }, 3000)
+        
+    })
+})
+
+
 
 
